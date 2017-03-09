239f902437abd3878634cf18397c891a0907c445
antoinewdg <antoinewendlinger@gmail.com>
2017-03-09 11:20:02 +0100

    Clean up files

--- python/flush_all_fast_outs.py
+++
@@ -1,6 +0,0 @@
-import glob
-
-from consts import *
-
-file_names = glob.glob(OUTPUT_DIR + "/**/*.*")
-print(file_names)
--- python/visualize_local_noise.py
+++
@@ -1,6 +0,0 @@
-from consts import *
-
-data = np.loadtxt(OUTPUT_DIR + "/noise_hist.txt")
-print("Global noise level=%s" % data[0])
-plt.hist(data[1:], bins=20)
-plt.show()
